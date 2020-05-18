1066263288ba158a65fa719d5d6de8c3cfc87e51
Levan Apakidze <lapak18@freeuni.edu.ge>
2020-05-18 19:10:46 +0400

    possible player class impl

---
+++ Startup/src/Player.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Player {
+    private List<Card> cards;
+    private final int id;
+    public Player(int id){
+        this.id = id;
+    }
+    public List<Card> validCards(Card currentHighest){
+        return null;
+    }
+
+    public void removeCard(Card chosen){
+
+    }
+    public void getDealtCards(List<Card> dealtCards){
+        cards = dealtCards;
+    }
+}
