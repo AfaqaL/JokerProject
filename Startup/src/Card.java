359c3ce87a39a49253f364514261e78c0e580dcc
tatena <ntate18@freeuni.edu.ge>
2020-05-21 00:09:19 +0400

    Card compare, valid done, hopefully

--- Startup/src/Card.java
+++ Startup/src/Card.java
@@ -1,9 +1,10 @@
-public class Card{
+public class Card implements Comparable{
 
     public static final int VALID_COLORS = 0;
     public static final int VALID_SUPERIOR = 1;
 
 
+    public static final int NO_COLOR = -1;
     public static final int CLUBS = 0;
     public static final int DIAMONDS = 1;
     public static final int SPADES = 2;
@@ -47,7 +48,7 @@ public class Card{
      * @param first
      * @return
      */
-    boolean isValid(Card first, int mode) {
+    boolean isValid(Card first) {
         return valid;
     }
 
@@ -56,10 +57,21 @@ public class Card{
     }
 
 
+    /**
+     *
+     * @param taker
+     * @param superior
+     * @return 1 if this > taker, -1 if this < taker
+     */
     public int compare(Card taker, int superior) {
         if (taker instanceof JokerCard)  { return compareJoker((JokerCard)taker, superior); }
 
-        if (taker.color == this.color)  { return 1; }
+        if (taker.color == this.color)  {
+            if (this.value > taker.value)
+                return 1;
+            else
+                return -1;
+        }
 
         if (taker.color == superior) { return -1; }
 
@@ -69,6 +81,25 @@ public class Card{
     }
 
     private int compareJoker(JokerCard taker, int superior) {
+        switch (taker.mode) {
+            case JokerCard.OVER_MODE:
+                return -1;
+            case JokerCard.GIVE_HIGHEST_MODE:
+                if (this.color == superior && taker.color != superior)
+                    return 1;
+                else
+                    return -1;
+            case JokerCard.TAKE_MODE:
+                if (this.color == taker.color || this.color == superior)
+                    return 1;
+                else
+                    return -1;
+        }
         return -1;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return ((Card)o).value - this.value;
     }
 }
