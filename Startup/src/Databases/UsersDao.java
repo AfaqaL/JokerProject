10902c60282aea6d5867e8916056050c27e43119
gkhelo <gkhel18@freeuni.edu.ge>
2020-05-25 22:11:09 +0400

    implementation of UsersSqlDao class and tests

--- Startup/src/Databases/UsersDao.java
+++ Startup/src/Databases/UsersDao.java
@@ -2,18 +2,84 @@ package Databases;
 
 import HelperClasses.User;
 
-public interface UsersDao {
-    boolean createTable();
+import java.sql.SQLException;
 
+public interface UsersDao {
+    /**
+     * Creates a new table. Throws
+     * sqlException if table can't
+     * be created.
+     */
+    void createTable() throws SQLException;
+
+    /**
+     * Drops table. Throws sqlException
+     * if table can't be dropped.
+     */
+    void dropTable() throws SQLException;
+
+    // Convenience methods to search users
+    // in the database.
+    /**
+     * Search user in the database
+     * using id of user.
+     * @param id - unique number for user
+     * @return found user
+     */
     User searchById(long id);
 
+    /**
+     * Search user in the database
+     * using mail of user.
+     * @param mail - user mail
+     * @return found user
+     */
     User searchByMail(String mail);
 
+    /**
+     * Search user in the database
+     * using username of user.
+     * @param username - name of user
+     * @return found user
+     */
     User searchByUsername(String username);
 
+    /**
+     * Search user in the database
+     * using usernama and password.
+     * @param username - name of user
+     * @param password - user password
+     * @return found user
+     */
     User searchByUsernameAndPassword(String username, String password);
 
+    /**
+     * Adds new user in the database
+     * @param username - name of user
+     * @param password - user password
+     * @param mail - user mail
+     * @return true if user is added
+     * in the database, false - otherwise
+     */
     boolean addUser(String username, String password, String mail);
 
-    boolean changePassword(long id);
+    // Convenience methods to update user's
+    // information in the database.
+    /**
+     * Changes password for the given user
+     * @param username - name of user
+     * @param newPassword - new password for user
+     * @return true if password changed,
+     * false - otherwise
+     */
+    boolean changePassword(String username, String newPassword);
+
+    /**
+     * Changes rank for the given user
+     * @param username - name of user
+     * @param newRank - new rank for user
+     * @return true if rank updated,
+     * false otherwise
+     */
+    boolean changeRank(String username, int newRank);
 }
--- Startup/src/Databases/UsersSqlDao.java
+++ Startup/src/Databases/UsersSqlDao.java
@@ -2,40 +2,173 @@ package Databases;
 
 import HelperClasses.User;
 
+import java.sql.*;
+
 public class UsersSqlDao implements UsersDao {
+    private Connection connection;
+
+    public UsersSqlDao(Connection connection) {
+        this.connection = connection;
+    }
 
     @Override
-    public boolean createTable() {
-        return false;
+    public void createTable() throws SQLException {
+        Statement stm = connection.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS users (\n" +
+                "    user_id BIGINT NOT NULL AUTO_INCREMENT,\n" +
+                "    username VARCHAR(20) NOT NULL UNIQUE,\n" +
+                "    mail VARCHAR(32) NOT NULL UNIQUE,\n" +
+                "    password VARCHAR(20) NOT NULL,\n" +
+                "    rank INT NOT NULL,\n" +
+                "    PRIMARY KEY (user_id)\n" +
+                ");");
+    }
+
+    @Override
+    public void dropTable() throws SQLException {
+        Statement stm = connection.createStatement();
+        stm.execute("DROP TABLE IF EXISTS users;");
     }
 
     @Override
     public User searchById(long id) {
-        return null;
+        try {
+            PreparedStatement query = connection.prepareStatement(
+                    "SELECT * FROM users WHERE user_id = ?;"
+            );
+            query.setLong(1, id);
+
+            ResultSet rs = query.executeQuery();
+            if (!rs.next()) return null;
+
+            String username = rs.getString(2);
+            String mail = rs.getString(3);
+            String password = rs.getString(4);
+            int rank = rs.getInt(5);
+
+            return new User(id, username, mail, password, rank);
+        } catch (SQLException e) {
+            return null;
+        }
     }
 
     @Override
     public User searchByMail(String mail) {
-        return null;
+        try {
+            PreparedStatement query = connection.prepareStatement(
+                    "SELECT * FROM users WHERE mail = ?;"
+            );
+            query.setString(1, mail);
+
+            ResultSet rs = query.executeQuery();
+            if (!rs.next()) return null;
+
+            long id = rs.getLong(1);
+            String username = rs.getString(2);
+            String password = rs.getString(4);
+            int rank = rs.getInt(5);
+
+            return new User(id, username, mail, password, rank);
+        } catch (SQLException e) {
+            return null;
+        }
     }
 
     @Override
     public User searchByUsername(String username) {
-        return null;
+        try {
+            PreparedStatement query = connection.prepareStatement(
+                    "SELECT * FROM users WHERE username = ?;"
+            );
+            query.setString(1, username);
+
+            ResultSet rs = query.executeQuery();
+            if (!rs.next()) return null;
+
+            long id = rs.getLong(1);
+            String mail = rs.getString(3);
+            String password = rs.getString(4);
+            int rank = rs.getInt(5);
+
+            return new User(id, username, mail, password, rank);
+        } catch (SQLException e) {
+            return null;
+        }
     }
 
     @Override
     public User searchByUsernameAndPassword(String username, String password) {
-        return null;
+        try {
+            PreparedStatement query = connection.prepareStatement(
+                    "SELECT * FROM users\n" +
+                            "WHERE username = ? AND\n" +
+                            "    password = ?;"
+            );
+            query.setString(1, username);
+            query.setString(2, password);
+
+            ResultSet rs = query.executeQuery();
+            if (!rs.next()) return null;
+
+            long id = rs.getLong(1);
+            String mail = rs.getString(3);
+            int rank = rs.getInt(5);
+
+            return new User(id, username, mail, password, rank);
+        } catch (SQLException e) {
+            return null;
+        }
     }
 
     @Override
     public boolean addUser(String username, String password, String mail) {
-        return false;
+        try {
+            PreparedStatement insert = connection.prepareStatement(
+                    "INSERT INTO users (username, mail, password, rank)\n" +
+                            "VALUES (?, ?, ?, 0);"
+            );
+            insert.setString(1, username);
+            insert.setString(2, mail);
+            insert.setString(3, password);
+
+            insert.execute();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
     }
 
     @Override
-    public boolean changePassword(long id) {
-        return false;
+    public boolean changePassword(String username, String newPassword) {
+        try {
+            PreparedStatement update = connection.prepareStatement(
+                    "UPDATE users SET password = ?\n" +
+                            "WHERE username = ?;"
+            );
+            update.setString(1, newPassword);
+            update.setString(2, username);
+
+            update.execute();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean changeRank(String username, int newRank) {
+        try {
+            PreparedStatement update = connection.prepareStatement(
+                    "UPDATE users SET rank = ?\n" +
+                            "WHERE username = ?;"
+            );
+            update.setLong(1, newRank);
+            update.setString(2, username);
+
+            update.execute();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
     }
 }
