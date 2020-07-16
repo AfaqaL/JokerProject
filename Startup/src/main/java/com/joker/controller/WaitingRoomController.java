137a2162078c05755032fb538ea35406ec85daa0
tatena <ntate18@freeuni.edu.ge>
2020-07-16 13:27:50 +0400

    createWaiting room Controller updates

--- Startup/src/main/java/com/joker/controller/WaitingRoomController.java
+++ Startup/src/main/java/com/joker/controller/WaitingRoomController.java
@@ -3,14 +3,18 @@ package com.joker.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.joker.dao.InMemoryWaitingRoomDao;
 import com.joker.managers.WaitingRoomManager;
 import com.joker.managers.WaitingRoomManagerBean;
+import com.joker.model.GameMode;
 import com.joker.model.Room;
+import com.joker.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping
@@ -29,16 +33,21 @@ public class WaitingRoomController {
     }
 
     @PostMapping("/waitingRoom/create")
-    public @ResponseBody
-    String createTable(HttpServletRequest req,
-                       @RequestBody String waitingRoom) throws JsonProcessingException {
+    public @ResponseBody long createTable(HttpSession session,
+                                            @RequestBody String waitingRoom) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Gson converter = new Gson();
-        Room user = converter.fromJson(waitingRoom, Room.class);
-        System.out.println(user.getPassword() + "Sssssssssssssssssss");
-        return mapper.writeValueAsString(req.getSession().getAttribute("user"));
+        Room roomData = converter.fromJson(waitingRoom, Room.class);
+        User creator = (User)session.getAttribute("user");
+        String password = roomData.getPassword();
+        int bayonet = roomData.getBayonet();
+        GameMode gameMode = roomData.getGameMode();
 
+        long id = waitingRoomManager.createWaitingRoom(creator, password, bayonet, gameMode);
+
+        return id;
     }
+
 
     @PostMapping("/waitingRoom/join")
     public @ResponseBody String joinTable(){
