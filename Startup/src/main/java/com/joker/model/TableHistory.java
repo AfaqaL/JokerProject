20edac817ae6a46500dca0625f3080eb3814f551
gkhelo <gkhel18@freeuni.edu.ge>
2020-07-26 15:28:51 +0400

    added tests for HistorySqlDao

--- Startup/src/main/java/com/joker/model/TableHistory.java
+++ Startup/src/main/java/com/joker/model/TableHistory.java
@@ -5,6 +5,8 @@ public class TableHistory {
     private long id1, id2, id3, id4;
     private double score1, score2, score3, score4;
 
+    public TableHistory() {}
+
     public TableHistory(long tableId, long id1, double score1,
                         long id2, double score2,
                         long id3, double score3,
@@ -26,36 +28,72 @@ public class TableHistory {
         return tableId;
     }
 
+    public void setTableId(long tableId) {
+        this.tableId = tableId;
+    }
+
     public long getId1() {
         return id1;
+    }
+
+    public void setId1(long id1) {
+        this.id1 = id1;
     }
 
     public long getId2() {
         return id2;
     }
 
+    public void setId2(long id2) {
+        this.id2 = id2;
+    }
+
     public long getId3() {
         return id3;
+    }
+
+    public void setId3(long id3) {
+        this.id3 = id3;
     }
 
     public long getId4() {
         return id4;
     }
 
+    public void setId4(long id4) {
+        this.id4 = id4;
+    }
+
     public double getScore1() {
         return score1;
+    }
+
+    public void setScore1(double score1) {
+        this.score1 = score1;
     }
 
     public double getScore2() {
         return score2;
     }
 
+    public void setScore2(double score2) {
+        this.score2 = score2;
+    }
+
     public double getScore3() {
         return score3;
     }
 
+    public void setScore3(double score3) {
+        this.score3 = score3;
+    }
+
     public double getScore4() {
         return score4;
+    }
+
+    public void setScore4(double score4) {
+        this.score4 = score4;
     }
 
     @Override
