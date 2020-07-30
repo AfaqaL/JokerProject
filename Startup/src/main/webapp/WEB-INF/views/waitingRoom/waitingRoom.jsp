f951d1f95c897ac7e265f168574c5e1784e950ff
groga18 <62848696+groga18@users.noreply.github.com>
2020-07-30 14:40:34 +0400

    waiting room front

--- Startup/src/main/webapp/WEB-INF/views/waitingRoom/waitingRoom.jsp
+++ Startup/src/main/webapp/WEB-INF/views/waitingRoom/waitingRoom.jsp
@@ -12,32 +12,133 @@
 <%--    <script><%@include file="../../../resources/JS/waitingRoomFunctional.js"%></script>--%>
     <script src="${pageContext.request.contextPath}/resources/JS/waitingRoomFunctional.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+
 </head>
 <body onload="fetchData()">
     <h1> The ყომარბაზი </h1>
-    <div id="existingTables">
-        <!-- created tables will be added here! -->
+    <div id="existingTables1">
+        <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+        <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
-<%--        <button onclick="enterTable()" id="enter">Enter Table</button> <br><br><br>--%>
-    </div> <br>
+        <!DOCTYPE html>
 
+        <head>
+            <%--    <meta name="viewport" content="width = device-width, initial scale=1.0">--%>
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>smth</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script><title> waiting </title>
+            <script src="${pageContext.request.contextPath}/resources/JS/waitingRoomFunctional.js" type="text/javascript"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 
-    <div id = "createTableDiv" style="float:right">
-        <label for="bayonetValue">ხიშტი: </label>
-        <input type = "number" id = "bayonetValue"> <br>
-        <label for="passwordValue">პაროლი: </label>
-        <input type = "text" id = "passwordValue"> <br>
-        <label for="tableType">აირჩიე თამაშის ტიპი: </label> <br>
-        <div id="tableType">
-            <input type="radio" name="gameMode" id="standard"> სტანდარტული<br>
-            <input type="radio" name="gameMode" id="nines"> ცხრიანები <br>
+            <!-- jQuery library -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+            <!-- Popper JS -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+            <!-- Latest compiled JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+            <link href="<c:url value="/resources/CSS/waitingRoom.css" />" rel="stylesheet">
+
+        </head>
+
+        <body  onload="fetchData()" >
+        <nav class="navbar navbar-fixed-top black-gradients">
+            <div class="container" style="position: relative">
+                <label> The ყომარბაზი </label>
+                <%--        <div class="avatar-menu">--%>
+                <%--            <div class="dropdown">--%>
+                <%--                <div class="image-shadow dropdown-toggle" type="button" data-toggle="dropdown">--%>
+                <%--                    <img class="img-circle user-avatar" ng-src="https://graph.facebook.com/1240524699485740/picture?type=square&amp;height=200" alt="jokerstars_joker_avatar" src="https://graph.facebook.com/1240524699485740/picture?type=square&amp;height=200">--%>
+                <%--                </div>--%>
+
+                <%--            </div>--%>
+                <a class="logOut" href="/login"> გამოსვლა </a>
+                <%--        </div>--%>
+            </div>
+        </nav>
+        <!-- Modal -->
+        <div class="modal fade" id="create" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div id = "createTableDiv" >
+                                <label for="bayonetValue">ხიშტი: </label>
+                                <input type = "number" id = "bayonetValue"> <br>
+                                <label for="passwordValue">პაროლი: </label>
+                                <input type = "text" id = "passwordValue"> <br>
+                                <label for="tableType">აირჩიე თამაშის ტიპი: </label> <br>
+                                <div id="tableType">
+                                    <input type="radio" name="gameMode" id="standard"> სტანდარტული<br>
+                                    <input type="radio" name="gameMode" id="nines"> ცხრიანები <br>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" data-dismiss="modal" onclick = "createTable()">Create Table</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <button onclick="createTable()" id="create">Create Table</button>
-    </div>
 
-    <div id = "histories">
-        <a href="/histories"> თამაშების ისტორიის ნახვა </a>
-    </div>
+        <div class="container">
+            <div class="jumbotron">
+                <div class="card">
 
-</body>
+                    <div class="card-body">
+                        <h5 class="card-title">მაგიდები</h5>
+
+                        <div class="row">
+                            <div class="col-md-12 text-right">
+                                <button type="button" class="btn btn-warning badge-pill " data-toggle="modal" data-target="#create" id = "createTable">მაგიდის მასპინძლობა</button>
+                            </div>
+                        </div>
+                        <table id="existingTables" class="table table-bordered table-dark">
+                            <thead>
+                            <tr>
+                                <th scope="col">მასპინძელი</th>
+                                <th scope="col">ხიშტი</th>
+                                <th scope="col">ტიპი</th>
+                                <th scope="col">პაროლი</th>
+                                <th scope="col">მომხმარებლები</th>
+                                <th>შებრძანება</th>
+                            </tr>
+                            </thead>
+                            <tbody id="rooms">
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </body>
+
 </html>
--- Startup/src/main/webapp/resources/JS/waitingRoomFunctional.js
+++ Startup/src/main/webapp/resources/JS/waitingRoomFunctional.js
@@ -1,54 +1,5 @@
-
 function createTable() {
     let req = new XMLHttpRequest();
-    req.onreadystatechange = function() {
-        if(this.readyState === 4) {
-            if (this.status === 200) {
-
-                /* get the response (new table's id as string) */
-                let data = this.responseText;
-
-                /* div for new table */
-                let innerDiv = document.createElement("div");
-                innerDiv.setAttribute("id", "div" + data);
-
-                /* table describtion */
-                let childLabel = document.createElement("label");
-                childLabel.innerHTML = "ხიშტი: " + tableBayonet + "," + tableGameMode + "მოთამაშეები: 0 ";
-
-                /* table password input */
-                let childInput = document.createElement("input");
-                childInput.setAttribute("type", "text");
-                childInput.setAttribute("id", "joinPass" + data);
-
-                /* table describtion */
-                let childButton = document.createElement("button");
-                childButton.setAttribute("id", data);
-                childButton.setAttribute("onclick", "joinTable(this.id)");
-                childButton.setAttribute("class", "join-buttons");
-                childButton.innerHTML = "Join Table";
-
-                /* adding all components to table div */
-                innerDiv.appendChild(childLabel);
-                innerDiv.appendChild(childInput);
-                innerDiv.appendChild(childButton);
-                innerDiv.style.border = "thick solid #0000FF";
-
-                /* adding table to existing tables and removing create table UI */
-                document.getElementById("existingTables").appendChild(innerDiv);
-                document.getElementById("createTableDiv").innerHTML = "";
-
-                /* disable all login buttons */
-                let allJoinButtons = document.getElementsByClassName("join-buttons");
-                [].forEach.call(allJoinButtons, (joinBtn) => {
-                    joinBtn.setAttribute("disabled", "disabled");
-                })
-            } else {
-                console.log(this.statusText);
-            }
-        }
-    };
-
     req.open("POST", "/waitingRoom/create", true);
     req.setRequestHeader('Content-Type', 'application/json');
 
@@ -64,9 +15,8 @@ function createTable() {
         tableGameMode = 'STANDARD';
     }
     let waitingRoomData = JSON.stringify({id: id, password: tablePass, bayonet: tableBayonet,
-                                                gameMode: tableGameMode, players: []});
+        gameMode: tableGameMode, players: []});
     req.send(waitingRoomData);
-
 }
 
 function joinTable(table_id) {
@@ -76,20 +26,8 @@ function joinTable(table_id) {
         if(this.readyState === 4) {
             if (this.status === 200) {
                 let respData = this.responseText;
-                if (respData === "TRUE") {
-
-                    /* highlight the table to join and disable all other join buttons */
-                    document.getElementById("div" + table_id).style.border = "thick solid #0000FF";
-                    let allJoinButtons = document.getElementsByClassName("join-buttons");
-                    [].forEach.call(allJoinButtons, (joinBtn) => {
-                        joinBtn.setAttribute("disabled", "disabled");
-                    })
-
-                    document.getElementById("createTableDiv").innerHTML = "";
-
-                } else {
-                    console.log(respData);
-                    alert("Sorry! Could not join the table");
+                if (respData !== "TRUE") {
+                    alert("Sorry! Could not join the table, try again");
                 }
             }
         }
@@ -102,8 +40,6 @@ function joinTable(table_id) {
     req.send(data);
 }
 
-
-
 function fetchData() {
     setInterval(function () {
         let req = new XMLHttpRequest();
@@ -113,48 +49,61 @@ function fetchData() {
                 if (this.status === 200) {
                     let respData = JSON.parse(this.responseText);
                     console.log(respData);
-                    if(respData.isChanged === "TRUE") {
+                    if (respData.isChanged === "TRUE") {
 
                         /* new data incoming! */
-                        console.log("Changed");
-                        document.getElementById("existingTables").innerHTML ="";
-
+                        console.log("Channfvvtgbthtgged");
+                        //                  document.getElementById("rooms").innerHTML = "";
+                        $("#rooms").empty();
+                        //                  console.log("baroo");
                         /* new list of rooms (some removed, some added) */
                         [].forEach.call(respData.rooms, (room) => {
 
-                            let innerDiv = document.createElement("div");
-                            innerDiv.setAttribute("id", "div" + room.id);
+                            let row = document.createElement("tr");
+                            row.setAttribute("id", "room" + room.id);
 
-                            if (respData.tableId === room.id) {
-                                innerDiv.style.border = "thick solid #0000FF";
-                                if (room.players.length === 4) {
-                                    window.location.href = "/table"
-                                }
-                            }
+                            let col1 = document.createElement("td");
+                            let col2 = document.createElement("td");
+                            let col3 = document.createElement("td");
+                            let col4 = document.createElement("td");
+                            let col5 = document.createElement("td");
+                            let col6 = document.createElement("td");
 
-                            /* re-label */
-                            let childLabel = document.createElement("label");
-                            childLabel.innerHTML = "ხიშტი: " + room.bayonet + "," + room.gameMode + "მოთამაშეები:" + room.players.length;
 
-                            /* password field */
-                            let childInput = document.createElement("input");
-                            childInput.setAttribute("type", "text");
-                            childInput.setAttribute("id", "joinPass" + room.id);
+                            let inputElement = document.createElement("input");
+                            inputElement.setAttribute("type", "password");
+                            inputElement.setAttribute("id", "joinPass" + room.id);
 
                             let childButton = document.createElement("button");
                             childButton.setAttribute("id", room.id);
                             childButton.setAttribute("onclick", "joinTable(this.id)");
-                            childButton.setAttribute("class", "join-buttons");
-                            childButton.innerHTML = "Join Table";
+                            childButton.setAttribute("class", "btn btn-primary badge-pill");
+                            childButton.innerHTML = "შებრძანება";
+
                             if (respData.tableId !== -1) {
                                 childButton.setAttribute("disabled", "disabled");
                             }
 
-                            /* add all the components to inner and main divs */
-                            innerDiv.appendChild(childLabel);
-                            innerDiv.appendChild(childInput);
-                            innerDiv.appendChild(childButton);
-                            document.getElementById("existingTables").appendChild(innerDiv);
+                            if (respData.tableId !== -1) {
+                                childButton.setAttribute("disabled", "disabled");
+                                document.getElementById("createTable").setAttribute("disabled", "disabled");
+                            }
+
+                            col1.innerHTML = room.players[0].username;
+                            col2.innerHTML = room.bayonet;
+                            col3.innerHTML = room.gameMode;
+                            col4.appendChild(inputElement);
+                            col5.innerHTML = room.players.length + "";
+                            col6.appendChild(childButton);
+
+                            row.appendChild(col1);
+                            row.appendChild(col2);
+                            row.appendChild(col3);
+                            row.appendChild(col4);
+                            row.appendChild(col5);
+                            row.appendChild(col6);
+
+                            document.getElementById("rooms").appendChild(row);
                             /* end */
                         });
 
@@ -162,6 +111,7 @@ function fetchData() {
                         console.log("Stays Same")
                     }
                 }
+
             }
         };
 
@@ -169,5 +119,5 @@ function fetchData() {
         req.setRequestHeader('Content-Type', 'application/json');
         req.send();
 
-    }, 3000);
+    }, 1500);
 }
