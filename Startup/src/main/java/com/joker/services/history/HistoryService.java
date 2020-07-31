252281f49071b7514282c9220bd8b4e9030c93a8
gkhelo <gkhel18@freeuni.edu.ge>
2020-07-31 23:01:34 +0400

    refactored histories room

--- Startup/src/main/java/com/joker/services/history/HistoryService.java
+++ Startup/src/main/java/com/joker/services/history/HistoryService.java
@@ -1,12 +1,13 @@
 package com.joker.services.history;
 
 import com.joker.model.TableHistory;
+import com.joker.model.dto.HistoryResponse;
 
 import java.util.List;
 
 public interface HistoryService {
 
-    List<TableHistory> getUserHistory(long id);
+    List<HistoryResponse> getUserHistory(long id);
 
     boolean addHistory(TableHistory history);
 }
